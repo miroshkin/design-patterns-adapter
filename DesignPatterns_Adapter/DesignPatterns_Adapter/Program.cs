95704eb378164723358ac3775b8f140882919fdd
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-28 12:46:47 +0300

    Added project

---
+++ DesignPatterns_Adapter/DesignPatterns_Adapter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns_Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
