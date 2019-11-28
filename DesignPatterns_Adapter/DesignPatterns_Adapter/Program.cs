2ff67125a59917b764d6565004ca49fe9067cab7
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-28 12:58:41 +0300

    Added adapter example

--- DesignPatterns_Adapter/DesignPatterns_Adapter/Program.cs
+++ DesignPatterns_Adapter/DesignPatterns_Adapter/Program.cs
@@ -2,11 +2,71 @@
 
 namespace DesignPatterns_Adapter
 {
+    //original source https://metanit.com/sharp/patterns/4.2.php
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Design Patterns - Adapter!");
+
+            Driver driver = new Driver();
+
+            Car car = new Car();
+            driver.Travel(car);
+
+            Camel camel = new Camel();
+            ITransport camelTransport = new CamelToTransportAdapter(camel);
+            driver.Travel(camelTransport);
         }
     }
+
+    class Driver 
+    {
+        public void Travel(ITransport transport)
+        {
+            transport.Drive();
+        }
+    }
+
+    class Camel : IAnimal
+    {
+        public void Move()
+        {
+            Console.WriteLine("Camel is moving");
+        }
+    }
+
+    class Car : ITransport
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving");
+        }
+    }
+
+    interface ITransport
+    {
+        void Drive();
+    }
+
+    interface IAnimal
+    {
+        void Move();
+    }
+
+    class CamelToTransportAdapter: ITransport
+    {
+        private Camel _camel;
+
+        public CamelToTransportAdapter(Camel camel)
+        {
+            _camel = camel;
+        }
+
+        public void Drive()
+        {
+            _camel.Move();
+        }
+    }
+
 }
